863861e20c2603e9489cfa813ada0efd1f2b6a8f
sanskar <sanskarvns19@gmail.com>
2026-05-16 20:44:30 +0530

    deploy

--- src/main/java/com/sanskar/crawler/controller/HelloController.java
+++ src/main/java/com/sanskar/crawler/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "hello from CI/CD pipeline";
+        return "hello from CI/CD pipeline......";
     }
 }
